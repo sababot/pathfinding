86711fb7cffb6b511b75f4a45bb5acba5bf40932
sababot <twinsgardos@gmail.com>
2021-09-11 21:59:25 +0000

    asdasf

---
+++ astar/src/board.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int y = 0; y < 20; i++)
+	{
+		for (int x = 0; x < 20; j++)
+		{
+			if (y = 0 && x != 20)
+				cout << "#";
+			if (x = 20)
+				cout << "\n";
+			if (y != 0 && y != 20 && 
+		}
+	}
+}
